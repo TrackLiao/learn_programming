fc08558e81461914bfffe89ebeddba87890b0497
Chakyu Liew <track.liao@gmail.com>
2020-11-05 16:19:07 +0800

    add condition code

--- c_code/Conditions.c
+++ c_code/Conditions.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+void guessNumber(int guess) {
+    if (guess == 555) {
+        printf("Correct. You guessed it\n");
+    } else if (guess <= 555) {
+        printf("Your guess is too low.\n");
+    } else {
+        printf("Your guess is too high\n");
+    }
+}
 
 int main()
 {
-    printf("Hello world\n");
+    guessNumber(500);
+    guessNumber(600);
+    guessNumber(555);
     return 0;
 }
 
