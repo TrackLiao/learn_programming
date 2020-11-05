10c86999d7ec8ab86765d89a807a0e8481024b48
Chakyu Liew <track.liao@gmail.com>
2020-11-05 16:36:43 +0800

    add for loop code

---
+++ c_code/Strings.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+/* char with pointer, read-only */
+/* char * name = "John Smith"; */
+
+/* local character array */
+/* char name[] = "John Smith"; */
+
+/* String Length */
+/* strlen(name) */
+
+/* String comparison */
+/* strncmp(A, B, n); */
+
+/* String Concatenation */
+/* strncat(A, B, max_append_digit); */
+int main()
+{
+    char * first_name = "John";
+
+    char last_name[] = "Doe";
+
+    char name[100];
+    last_name[0] = 'B';
+    sprintf(name, "%s %s", first_name, last_name);
+    if (strncmp(name, "John Boe", 100) == 0) {
+        printf("Done!\n");
+
+    }
+
+    name[0] = '\0';
+    strncat(name,first_name, 4);
+    strncat(name,last_name, 20);
+    printf("%s\n", name);
+    return 0;
+}
+
